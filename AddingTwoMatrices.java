e0a568942c987c4113025d56a1102c73d8a4b97b
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-09-19 05:26:31 +0000

    make some files

---
+++ AddingTwoMatrices.java
@@ -0,0 +1,15 @@
+public class AddingTwoMatrices{
+   public static void main(String args[]){
+      int a[][]={{1,2,3},{4,5,6},{7,8,9}};
+      int b[][]={{1,1,1},{1,1,1},{1,1,1}};
+      int c[][]=new int[3][3];
+
+      for(int i = 0;i<3;i++){
+         for(int j = 0;j<3;j++){
+            c[i][j] = a[i][j]+b[i][j];
+            System.out.print(c[i][j]+" ");
+         }
+         System.out.println();
+      }
+   }
+}
