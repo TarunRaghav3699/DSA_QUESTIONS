db622a2b93d36a1f891b6b8bdb9e551ff5995386
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-09-20 06:35:24 +0530

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,92 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+    Node tail;
+
+    public LinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    // Insert at the beginning
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    // Insert at the end
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (tail == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // Insert at a specific position (middle)
+    public void insertAtMiddle(int data, int position) {
+        Node newNode = new Node(data);
+        if (position == 0) {
+            insertAtBeginning(data);
+            return;
+        }
+        Node current = head;
+        int index = 0;
+        while (current != null && index < position - 1) {
+            current = current.next;
+            index++;
+        }
+        if (current == null) {
+            System.out.println("Invalid position");
+            return;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+    }
+
+    // Display the linked list
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        // Insert at the beginning
+        list.insertAtBeginning(1);
+
+        // Insert at the end
+        list.insertAtEnd(3);
+
+        // Insert at the middle (position 1)
+        list.insertAtMiddle(2, 1);
+
+        // Display the linked list
+        list.display();
+    }
+}
