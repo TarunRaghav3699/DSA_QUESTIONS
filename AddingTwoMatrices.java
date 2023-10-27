0e85354eed7cde22dbe0bf34a5e8134fcc0f7f96
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-10-27 10:17:41 +0530

    Update AddingTwoMatrices.java

--- AddingTwoMatrices.java
+++ AddingTwoMatrices.java
@@ -20,3 +20,27 @@ public class AddingTwoMatrices{
 // 2 3 4 
 // 5 6 7 
 // 8 9 10 
+
+
+
+
+class Node {
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+    }
+}
