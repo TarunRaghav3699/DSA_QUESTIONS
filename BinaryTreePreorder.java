6fb9c93199a8a990264bd6d5ea1853a6b557f7d5
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-11-08 23:56:33 +0000

    .

---
+++ BinaryTreePreorder.java
@@ -0,0 +1,21 @@
+public class BinaryTreePreorder {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree{
+        
+    }
+    public static void main(String[] args) {
+        int Nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}
+    }
+    
+}
