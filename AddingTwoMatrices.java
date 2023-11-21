d70afd563feb50bc7713fdcd8900379a8cbeaad9
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-11-21 11:44:13 +0530

    Update AddingTwoMatrices.java

--- AddingTwoMatrices.java
+++ AddingTwoMatrices.java
@@ -20,28 +20,7 @@ public class AddingTwoMatrices{
 // 2 3 4 
 // 5 6 7 
 // 8 9 10 
-ublic class AddingTwoMatrices{
-   public static void main(String args[]){
-      int a[][]={{1,2,3},{4,5,6},{7,8,9}};
-      int b[][]={{1,1,1},{1,1,1},{1,1,1}};
-      int c[][]=new int[3][3];
 
-      for(int i = 0;i<3;i++){
-         for(int j = 0;j<3;j++){
-            c[i][j] = a[i][j]+b[i][j];
-            System.out.print(c[i][j]+" ");
-         }
-         System.out.println();
-      }
-   }
-}
-
-// output:-
-
-// AddingTwoMatrices 
-// 2 3 4 
-// 5 6 7 
-// 8 9 10 
 
 // The algorithm for the matrix addition code in Java can be broken down into these steps:
 
