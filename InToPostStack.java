458e2e5d9daff09ca92fd569e502e1b9e3492eb6
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-11-28 00:57:57 +0000

    added infix to postfix using stack

---
+++ InToPostStack.java
@@ -0,0 +1,53 @@
+import java.util.Stack;
+
+public class InToPostStack {
+    static int precedence(char c) {
+        switch (c) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+
+    static String infixToPostfix(String infix) {
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : infix.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                postfix.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    postfix.append(stack.pop());
+                }
+                stack.pop();
+            } else {
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+                    postfix.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            postfix.append(stack.pop());
+        }
+
+        return postfix.toString();
+    }
+
+    public static void main(String[] args) {
+        String infixExpression = "a+b*(c^d-e)^(f+g*h)-i";
+        String postfixExpression = infixToPostfix(infixExpression);
+        System.out.println("Infix Expression: " + infixExpression);
+        System.out.println("Postfix Expression: " + postfixExpression);
+    }
+}
