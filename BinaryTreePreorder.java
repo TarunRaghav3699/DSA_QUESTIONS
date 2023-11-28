691c21250070d3069f41a344ef8a3a4516263e05
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-11-28 02:19:17 +0000

    .

--- BinaryTreePreorder.java
+++ BinaryTreePreorder.java
@@ -1,35 +1,31 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    public TreeNode(int value) {
+        val = value;
+        left = right = null;
+    }
+}
+
 public class BinaryTreePreorder {
-    static class Node{
-        int data;
-        Node left;
-        Node right;
-
-        Node(int data){
-            this.data = data;
-            this.left = null;
-            this.right = null;
+    static void preorderTraversal(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.val + " ");
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
         }
     }
 
-    static class BinaryTree{
-        static int idx = -1;
-        public static Node buildTree(int nodes[]){
-            idx++;
-            if (nodes[idx] == -1){
-                return null;
-            }
-            Node newNode = new Node(nodes[idx]);
-            newNode.left = buildTree(nodes);
-            newNode.right = buildTree(nodes);
-
-            return newNode;
-        }
-    }
     public static void main(String[] args) {
-        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
-        BinaryTree tree = new BinaryTree();
-        Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        // Example binary tree
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.println("Preorder Traversal:");
+        preorderTraversal(root);
     }
-    
 }
