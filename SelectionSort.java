72492d5e4e8cd57dd28877ecab2a1adaedc1ab7e
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-12-06 08:10:55 +0530

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void selectionSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {64, 25, 12, 22, 11};
+        int n = arr.length;
+
+        System.out.println("Original array: " + Arrays.toString(arr));
+
+        selectionSort(arr);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
